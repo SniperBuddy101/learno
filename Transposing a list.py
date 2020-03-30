099c3a9fce7b4c175cf3234adc1dfa649600a7f1
shreyashkarnik <karnik.shreyash@gmail.com>
2020-03-30 20:30:11 +0530

    Transposing with list comprehension.

---
+++ Transposing a list.py
@@ -0,0 +1,5 @@
+my_list = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+
+result = [[my_list[b][i] for b in range(len(my_list))] for i in range(len(my_list[0]))]
+
+print(result)
